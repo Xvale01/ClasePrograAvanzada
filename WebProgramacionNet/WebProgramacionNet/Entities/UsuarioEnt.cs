36cc155086bda53b744bd335de558dddf3ce9703
Xvale01 <valeriagc012004@gmail.com>
2023-09-27 17:43:47 -0600

    Implementación de Razor en Vistas

---
+++ WebProgramacionNet/WebProgramacionNet/Entities/UsuarioEnt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace WebProgramacionNet.Entities
+{
+    public class UsuarioEnt
+    {
+
+        // Propiedad con un tipo de dato y nombre
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+
+    }
+}
